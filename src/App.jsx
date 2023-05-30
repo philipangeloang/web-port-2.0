e4a6173ac49f9c6bf103068cfb95818c6ecb5665
Philip <philipangeloang1@gmail.com>
2023-05-30 17:10:34 +0800

    Added react-icons and finished nav structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { AiOutlineMail } from "react-icons/ai";
+
 function App() {
   return (
     <>
       <section className="w-full p-4">
-        <nav className="max-w-7xl mx-auto bg-main-black"></nav>
+        <nav className="max-w-7xl mx-auto bg-main-black flex justify-between">
+          <div className="flex items-center gap-5">
+            <AiOutlineMail className="bg-[#1a1a1a] text-main-white border-2 rounded-full p-4 border-main-white/5 h-14 w-14" />
+            <span className="text-sm text-main-white/70">Get in Touch</span>
+          </div>
+          <div className="flex gap-4 items-center text-sm">
+            <div>Home</div>/<div>Services</div>/<div>Projects</div>/
+            <div>Resume</div>
+          </div>
+        </nav>
       </section>
     </>
   );
