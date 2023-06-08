4e2712146467c77c00fe7fdd59ff3c0f53be1274
Philip <philipangeloang1@gmail.com>
2023-06-08 16:05:07 +0800

    Project Carousel

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,13 +1,36 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper";
+import "swiper/css";
+import "swiper/css/bundle";
+
 const Projects = () => {
   return (
-    <div className="max-w-6xl mx-auto flex flex-col justify-center items-center">
-      <p className="text-main-white text-4xl heading-gradient">
-        Recent Projects
-      </p>
-      <div className="grid gap-4 grid-cols-12 w-full mt-20">
-        <div className="project-card"></div>
+    <>
+      <div className="max-w-6xl mx-auto flex flex-col justify-center items-center">
+        <p className="text-main-white text-4xl heading-gradient">
+          Recent Projects
+        </p>
       </div>
-    </div>
+
+      {/* Carousel */}
+
+      <Swiper
+        modules={[Autoplay]}
+        autoplay={true}
+        slidesPerView={7}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+      </Swiper>
+    </>
   );
 };
 
